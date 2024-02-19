722c34f8c4b9dcaf43a4cc9e8b56d07d1e936936
leonelborgesdev <leonel03t@gmail.com>
2024-02-19 11:22:11 -0400

    api pokemon database

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import fileupload from "express-fileupload";
+
+const app = express();
+
+//Middelwares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
+
+// app.use(router);
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import app from "./app.js";
+import { sequelize } from "./database/database.js";
+
+async function main() {
+  try {
+    const PORT = process.env.PORT || 3000;
+    await sequelize.sync({ force: false });
+    console.log("conexion exitosa");
+    app.listen(PORT);
+    console.log("Server is listening on port ", PORT);
+  } catch (error) {
+    console.log("error en la conexion", error);
+  }
+}
+
+main();
